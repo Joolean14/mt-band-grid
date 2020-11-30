20bf7563bc7aab42ef7656a4d8122e1ba3d87c12
julian <Joolean14>
2020-11-30 14:49:22 -0500

    Remiendo el formulario con colbon

--- form.js
+++ form.js
@@ -1,3 +1,74 @@
+// const form = document.getElementById('form');
+// const userName = document.getElementById('userName');
+// const mobile = document.getElementById('mobile');
+// const email = document.getElementById('email');
+// const eventType = document.getElementById('eventType');
+// const date = document.getElementById('date');
+// const agree = document.getElementById('agree');
+
+// form.addEventListener('submit', e => {
+//     e.preventDefault()
+//     checkInputs();
+//     errors = document.getElementsByClassName('error') // Cuenta los campos con error
+//     if (errors.length === 0){
+//         form.submit();
+//         // alert() o mensaje de que ya todo fue exitoso
+//         // limpiasForm()
+//     }
+// });
+
+// function checkInputs() {
+//     const userNameValue = userName.value.trim();
+//     const mobileValue = mobile.value.trim();
+//     const emailValue = email.value.trim();
+//     const eventTypeValue = eventType.value.trim();
+//     const dateValue = date.value.trim();
+
+//     if (userNameValue === '') {
+//         setErrorFor(userName, 'Nombre no puede estar vacío');
+//     } else {
+//         setSuccessFor(userName);
+//     }
+//     if (mobileValue === '') {
+//         setErrorFor(mobile, 'Celular no puede estar vacío');
+//     } else {
+//         setSuccessFor(mobile);
+//     }
+//     if (emailValue === '') {
+//         setErrorFor(email, 'Email no puede estar vacío');
+//     } else if (!isEmail(emailValue)) {
+//         setErrorFor(email, 'No es un correo válido');
+//     } else {
+//         setSuccessFor(email);
+//     }
+//     if (eventTypeValue === '') {
+//         setErrorFor(eventType, 'Tipo de evento no puede estar vacío');
+//     } else {
+//         setSuccessFor(eventType);
+//     }
+//     if (dateValue === '') {
+//         setErrorFor(date, 'Tipo de evento no puede estar vacío');
+//     } else {
+//         setSuccessFor(date);
+//     }    
+// }
+
+// function setErrorFor(input, message) {
+//     const formControl = input.parentElement;
+//     const small = formControl.querySelector('small');
+//     formControl.className = 'form-control error';
+//     small.innerText = message;
+// }
+
+// function setSuccessFor(input) {
+//     const formControl = input.parentElement;
+//     formControl.className = 'form-control success';
+// }
+
+// function isEmail(email) {
+//     return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(email);
+// }
+
 const form = document.getElementById('form');
 const userName = document.getElementById('userName');
 const mobile = document.getElementById('mobile');
@@ -9,11 +80,9 @@ const agree = document.getElementById('agree');
 form.addEventListener('submit', e => {
     e.preventDefault()
     checkInputs();
-    errors = document.getElementsByClassName('error') // Cuenta los campos con error
+    errors = document.getElementsByClassName('error');
     if (errors.length === 0){
         form.submit();
-        // alert() o mensaje de que ya todo fue exitoso
-        // limpiasForm()
     }
 });
 
@@ -24,31 +93,28 @@ function checkInputs() {
     const eventTypeValue = eventType.value.trim();
     const dateValue = date.value.trim();
 
-
-    if (userNameValue === '') {
-        setErrorFor(userName, 'Nombre no puede estar vacío');
+    if (userNameValue === '' ) {
+        setErrorFor(userName);
     } else {
         setSuccessFor(userName);
     }
     if (mobileValue === '') {
-        setErrorFor(mobile, 'Celular no puede estar vacío');
+        setErrorFor(mobile);
     } else {
         setSuccessFor(mobile);
     }
-    if (emailValue === '') {
-        setErrorFor(email, 'Email no puede estar vacío');
-    } else if (!isEmail(emailValue)) {
-        setErrorFor(email, 'No es un correo válido');
+    if (!isEmail(emailValue)) {
+        setErrorFor(email);
     } else {
         setSuccessFor(email);
     }
     if (eventTypeValue === '') {
-        setErrorFor(eventType, 'Tipo de evento no puede estar vacío');
+        setErrorFor(eventType);
     } else {
         setSuccessFor(eventType);
     }
     if (dateValue === '') {
-        setErrorFor(date, 'Tipo de evento no puede estar vacío');
+        setErrorFor(date);
     } else {
         setSuccessFor(date);
     }    
@@ -56,9 +122,7 @@ function checkInputs() {
 
 function setErrorFor(input, message) {
     const formControl = input.parentElement;
-    const small = formControl.querySelector('small');
     formControl.className = 'form-control error';
-    small.innerText = message;
 }
 
 function setSuccessFor(input) {
@@ -69,6 +133,12 @@ function setSuccessFor(input) {
 function isEmail(email) {
     return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(email);
 }
+
+
+
+
+
+
 
 
 
